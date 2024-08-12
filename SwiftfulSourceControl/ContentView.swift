93ebdad5bed0f68a3f84948b607f79e55d925e53
GRAFFITI.LAB <graffiti.laboratory@gmail.com>
2024-08-12 22:21:22 +0900

    [Clean] Reorder commit message documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -8,20 +8,24 @@
 /*
  COMMIT MESSAGES
  
- NEW FEATURE:
+ 팀에 합류하면 그들만의 규칙이 있다.
+ 규칙이 무엇인지 물어봐야 하고  커밋 메시지를 어떻게 처리하는지 확인해야 한다.
+ 
+ NEW FEATURE: 새로운 기능에 대한 정보. 기능 구축.
  [Feature] Description of the feature
  
- BUG IN PRODUCTION:
- [Patch] Description of patch
- 
- BUG NOT IN PRODUCTION:
+ BUG NOT IN PRODUCTION: 앱스토어에 올리지 않고 내부용으로 기능을 푸쉬 후 발견되는 문제점들.
  [Bug] Description of the bug
  
- MUNDANE TASKS: 매일 반복되고 별 다를 것 없는 평범한 일상과 관련된 작업.
+ RELEASE: 앱스토어에 프로덕션으로 푸시할 때 커밋 설정. 이 릴리스가 정확히 어디에 있는지 기록에서 볼 수 있도록 함.
+ [release] Description of release
+ 
+ BUG IN PRODUCTION: 제품 사용자에 의해 발견된 버그.
+ [Patch] Description of patch
+ 
+ MUNDANE TASKS: 매일 반복되고 별 다를 것 없는 평범한 일상과 관련된 작업. 문서 재정렬과 같은 일상적인 작업.
  [Clean] Description of changes
  
- RELEASE:
- [release] Description o release
  
  */
 
