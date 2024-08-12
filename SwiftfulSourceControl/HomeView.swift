ba120718850e97e4ef3d799a46dcb21e6e04d7d2
GRAFFITI.LAB <graffiti.laboratory@gmail.com>
2024-08-12 21:33:39 +0900

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by KIM JIN UK on 8/12/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
