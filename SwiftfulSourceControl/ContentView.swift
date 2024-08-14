25a533a573caa2a45d5e7044bf35c24d3ee1ec4b
GRAFFITI.LAB <graffiti.laboratory@gmail.com>
2024-08-14 10:30:59 +0900

    [Feature] Fix Swiftful Thinking text again

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -45,7 +45,7 @@ struct ContentView: View {
             Image(systemName: "heart.fill")
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
-            Text("Swiftful Thinking!")
+            Text("Swiftful!")
             
             Button("Subscribe") {
                 
