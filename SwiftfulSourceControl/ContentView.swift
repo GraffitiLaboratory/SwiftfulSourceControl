ba81b88d035d316eeaaca9890308bb7d28a1730d
GRAFFITI.LAB <graffiti.laboratory@gmail.com>
2024-08-14 14:21:30 +0900

    [Feature] Subscribe now added back

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -47,7 +47,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Swiftful!")
             
-            Button("Subscribe") {
+            Button("Subscribe now!") {
                 
             }
             
