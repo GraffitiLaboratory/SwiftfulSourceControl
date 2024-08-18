8ac97f5b91ec6b4e0619e506671d5b439b1749f0
GRAFFITI.LAB <graffiti.laboratory@gmail.com>
2024-08-18 09:39:01 +0900

    [Feature] Add ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by KIM JIN UK on 8/18/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Nick!")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
